a24ec3ced22d70f4b93f255c4e40c9d823d65a57
Deepali277 <89859259+Deepali277@users.noreply.github.com>
2022-12-15 16:38:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-
 def count_batteries_by_usage(cycles):
   for i in cycles:
     if i<310:
-      lowCount=lowCount+1
+      low_count=low_count+1
     if i<=929 and i>=310:
-      mediumCount=mediumCount+1
+      medium_count=medium_count+1
     if i>=930:
-      highCount=highCount+1
+      high_count=high_count+1
   return {
-    "lowCount": 0,
-    "mediumCount": 0,
-    "highCount": 0
+    "lowCount": low_count,
+    "mediumCount": medium_count,
+    "highCount": high_count
   }
 
 
